a1feecd5c8e6ede904f2a939baaf468ef6f776fe
arshiazt <arshiazabeti@gmail.com>
2026-08-24 16:32:29 +0330

    initialize django project with development & production settings

---
+++ config/settings/dev.py
@@ -0,0 +1,15 @@
+from decouple import config
+from .base import *
+
+DEBUG = config('DEBUG',cast=bool,default=True)
+ALLOWED_HOSTS += ['127.0.0.1','localhost']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        'USER':config('USER'),
+        'PASSWORD':config('PASSWORD'),
+        'PORT':config('PORT'),
+        'HOST':config('HOST')
+    }
+}
