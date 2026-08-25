7ff36d4c306ad72a5da65bbd8327ceb2c2ed12d7
arshiazt <arshiazabeti@gmail.com>
2026-08-25 18:10:02 +0330

    create user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self,phone,password=None,**extra_fields):
+        if not phone:
+            raise ValueError('The phone field must be set.')
+        user=self.model(phone=phone,**extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user 
+    
+    def create_user(self,phone,password=None,**extra_fields):
+        extra_fields.setdefault('is_staff',False)
+        extra_fields.setdefault('is_active',True)
+        return self._crete_user(phone,password,**extra_fields)
+    
+    def create_superuser(self,phone,password=None,**extra_fields):
+        extra_fields.setdefault('is_active',True)
+        extra_fields.setdefault('is_staff',True)
+        extra_fields.setdefault('is_superuser',True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('Superuser must have is_active=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(phone,password,**extra_fields)
+
+class User(AbstractBaseUser,PermissionsMixin):
+
+    phone=models.CharField(max_length=11,unique=True,verbose_name='شماره تلفن')
+    first_name = models.CharField(max_length=100,blank=True,null=True)
+    last_name = models.CharField(max_length=100,blank=True,null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    object=UserManager()
+    
+    USERNAME_FIELD='phone'
+    REQUIRED_FIELDS=[]
+
+    def __str__(self):
+        return self.phone
--- config/settings/base.py
+++ config/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -74,7 +75,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES : dict = {}
-
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
