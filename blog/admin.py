77238ac29d890a5d43169c86f0ba27737bcb162d
arshiazt <arshiazabeti@gmail.com>
2026-08-27 20:23:48 +0330

    Learning the custom admin panel for the blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = ('name','slug','description','is_active')
+    list_filter = ('is_active',)
+    search_fields = ('name','description')
+    readonly_fields = ('slug',)
+
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = ('category','slug','price')
+    list_editable = ('price',)
+    fieldsets = (
+        ('Name',{
+            'fields':('category','slug')
+        }),
+        ('Price',{
+            'fields':('price',)
+        }
+        ),
+    )
+
+admin.site.register(Category,CategoryAdmin)
