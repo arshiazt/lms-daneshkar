ffc441a3b39416126a7117d663184c00d49b3f53
arshiazt <arshiazabeti@gmail.com>
2026-08-28 04:51:24 +0330

    create cbv for list/detail profile

--- user/urls.py
+++ user/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('profiles/',profiles_list_view,name="profiles-list"),
-    path('profiles/<int:pk>',profile_detail,name="profile-detail"),
+    # path('profiles/',profiles_list_view,name="profiles-list"),
+    # path('profiles/<int:pk>',profile_detail,name="profile-detail"),
+    path('profiles/',ProfilesListView.as_view(),name="profiles-list"),
+    path('profiles/<int:pk>',ProfileDetailView.as_view(),name="profile-detail"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Profile
+from django.views.generic import ListView,DetailView
 
 # Create your views here.
 
@@ -10,3 +11,18 @@ def profiles_list_view(request):
 def profile_detail(request,pk):
     profile = get_object_or_404(Profile,pk=pk)
     return render(request,'profile-detail.html',{'profile':profile})
+
+class ProfilesListView(ListView):
+    model = Profile
+    queryset = Profile.objects.select_related('user')
+    # queryset = Profile.objects.filter(role__exact='Student')
+    # queryset = Profile.objects.filter(role__iexact='Student')
+    # queryset = Profile.objects.filter(role__contains='st')
+    # queryset = Profile.objects.filter(role__iexact='STudent')
+    template_name = 'profile-list.html'
+    context_object_name = 'profiles'
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = 'profile-detail.html'
+    context_object_name = 'profile'
