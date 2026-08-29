738ff02e20d06ecb4f31818027784e475a873430
arshiazt <arshiazabeti@gmail.com>
2026-08-29 04:45:56 +0330

    add message for login register and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import UserRegisterForm,UserLoginForm
 from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -11,7 +12,10 @@ def user_register_view(request):
         if form.is_valid():
             user = form.save()
             login(request,user)
+            messages.success(request,f'{user.first_name} create')
             return redirect('portfolio')
+        else:
+            messages.error(request,'cant create this user')
     form = UserRegisterForm()
     return render(request,'register.html',{'form':form})
 
@@ -22,10 +26,14 @@ def user_login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request,user)
+            messages.success(request,f'{user.first_name} login')
             return redirect('portfolio')
+        else:
+            messages.error(request,'Username or Password is incorrect')
     form = UserLoginForm()
     return render(request,'login.html',{'form':form})
 
 def user_logout_view(request):
     logout(request)
+    messages.success(request,'user logout')
     return redirect('portfolio')
