ec00f62c214c5cbc95999997ba71d5a07441f182
arshiazt <arshiazabeti@gmail.com>
2026-08-29 06:17:12 +0330

    create simpletag templatetag

---
+++ user/templatetags/my_tags.py
@@ -0,0 +1,8 @@
+from django import template
+from user.models import Profile
+
+register = template.Library()
+
+@register.simple_tag
+def last_profiles(count=2):
+    return Profile.objects.order_by('-created_at')[:count]
