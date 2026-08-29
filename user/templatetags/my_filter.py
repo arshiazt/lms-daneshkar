49696af719b9ec26df8f08e7cc121e9d90287781
arshiazt <arshiazabeti@gmail.com>
2026-08-29 06:10:17 +0330

    create filter templatetag

---
+++ user/templatetags/my_filter.py
@@ -0,0 +1,13 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='reverse_str')
+def reverse_str(value):
+    return value[::-1]
+
+@register.filter(name='format_ir')
+def format_ir(phone,delimiter='-'):
+    if len(phone) != 11 or not phone.isdigit():
+        return phone
+    return f'{phone[:4]}{delimiter}{phone[4:7]}{delimiter}{phone[7:]}'
