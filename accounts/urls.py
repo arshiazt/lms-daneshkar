bc7e59b38e2f2faa3408b07e8f485fc445ded354
arshiazt <arshiazabeti@gmail.com>
2026-08-31 04:19:33 +0330

    create register/login/logout api urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('register/',UserRegisterView.as_view(),name='register'),
-    path('login/',UserLoginView.as_view(),name='login'),
-    path('logout/',UserLogoutView.as_view(),name='logout'),
+    # path('register/',UserRegisterView.as_view(),name='register'),
+    # path('login/',UserLoginView.as_view(),name='login'),
+    # path('logout/',UserLogoutView.as_view(),name='logout'),
+    path('api/auth/register/',RegisterApiView.as_view(),name='register-api'),
+    path('api/auth/login/',CustomLoginApiView.as_view(),name='login-api'),
+    path('api/auth/refresh/',TokenRefreshView.as_view(),name='refresh-api'),
+    path('api/auth/logout/',LogoutApiView.as_view(),name='logout-api'),
 ]
