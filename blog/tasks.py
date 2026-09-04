57bcee9f7b55befd658ca318d909df7f43544541
arshiazt <arshiazabeti@gmail.com>
2026-09-04 03:57:58 +0330

    create new task for test & learn

--- blog/tasks.py
+++ blog/tasks.py
@@ -15,3 +15,19 @@ def say_hello(name):
 
 eta_time = datetime.utcnow() + timedelta(seconds=30)
 say_hello.apply_async(('arshia',),eta=eta_time)
+
+@shared_task
+def add(x,y):
+    return x + y
+
+@shared_task
+def multiply(result,factor):
+    return result * factor
+
+@shared_task
+def power(x):
+    return x**2
+
+@shared_task
+def summerize(results):
+    return sum(results)
--- config/celery.py
+++ config/celery.py
@@ -10,4 +10,9 @@ app.config_from_object('django.conf:settings',namespace='CELERY')
 
 app.conf.broker_url = "redis://127.0.0.1:6379/0"
 
+app.conf.update(
+    broker_url = 'redis://127.0.0.1:6379/0',
+    result_backend = 'redis://127.0.0.1:6379/5'
+)
+
 app.autodiscover_tasks()
